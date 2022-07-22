2d5da6fe30cbfcc936791bae413dbfd845fc2451
Hector Hugo Martinez Martinez <57997735+hh-mm77@users.noreply.github.com>
2022-07-21 20:54:59 -0500

    estructura

--- resources/js/Components/clientes.vue
+++ resources/js/Components/clientes.vue
@@ -3,7 +3,6 @@ import * as am5 from '@amcharts/amcharts5';
 import * as am5xy from '@amcharts/amcharts5/xy';
 import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
 
-
 export default {
   
 props: {
@@ -14,199 +13,243 @@ props: {
 
 name: 'Graph',
 
-mounted() {
-
-    let vm=this;
-        
-   // console.log(vm.clientes); //comprobamos si imprime todos los los clientes
- 
-    let clients = vm.clientes; //guardamos en una varibale los cliente spara iterarlos
-
-    let grupo_conceptos = vm.grupo_conceptos;
-
-    //console.log(grupo_conceptos);
-
-    let datos = vm.cantidades;
-    //console.log(datos);
+mounted(){
+  let vm=this;
+            
+      // console.log(vm.clientes); //comprobamos si imprime todos los los clientes
     
-    let root = am5.Root.new(this.$refs.chartdiv);
+        let clients = vm.clientes; //guardamos en una varibale los cliente spara iterarlos
 
-    root.setThemes([am5themes_Animated.new(root)]);
+        let grupo_conceptos = vm.grupo_conceptos;
 
-    let chart = root.container.children.push(
-      am5xy.XYChart.new(root, {
-         panX: false,
-         panY: false,
-         wheelX: "none",
-         wheelY: "none",
-         layout: root.verticalLayout
+        //console.log(grupo_conceptos);
+
+        let datos = vm.cantidades;
+        //console.log(datos);
+        
+        let root = am5.Root.new(this.$refs.chartdiv);
+
+        root.setThemes([am5themes_Animated.new(root)]);
+
+        let chart = root.container.children.push(
+          am5xy.XYChart.new(root, {
+            panX: false,
+            panY: false,
+            wheelX: "none",
+            wheelY: "none",
+            layout: root.verticalLayout
+          })
+        );
+
+
+    // Create axes and their renderers
+    var yRenderer = am5xy.AxisRendererY.new(root, {
+      visible: false,
+      minGridDistance: 20,
+      inversed: true
+    });
+
+    yRenderer.grid.template.set("visible", false);
+
+    var yAxis = chart.yAxes.push(
+      am5xy.CategoryAxis.new(root, {
+        renderer: yRenderer,
+        categoryField: "category"
+      })
+    );
+
+    var xRenderer = am5xy.AxisRendererX.new(root, {
+      visible: false,
+      minGridDistance: 30,
+      inversed: false,
+      opposite:true,
+    });
+
+    xRenderer.grid.template.set("visible", false, );
+
+    var xAxis = chart.xAxes.push(
+      am5xy.CategoryAxis.new(root, {
+        renderer: xRenderer,
+        categoryField: "category",
+
       })
     );
 
 
- // Create axes and their renderers
-var yRenderer = am5xy.AxisRendererY.new(root, {
-  visible: false,
-  minGridDistance: 20,
-  inversed: true
-});
+    // Create series
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/#Adding_series
+    var series = chart.series.push(
+      am5xy.ColumnSeries.new(root, {
+        calculateAggregates: true,
+        stroke: am5.color(0xffffff),
+        clustered: false,
+        xAxis: xAxis,
+        yAxis: yAxis,
+        categoryXField: "x",
+        categoryYField: "y",
+        valueField: "value",
+      
+      })
+    );
 
-yRenderer.grid.template.set("visible", false);
+    series.columns.template.setAll({
+      tooltipText: "{value}",
+      strokeOpacity: 1,
+      strokeWidth: 2,
+      cornerRadiusTL: 5,
+      cornerRadiusTR: 5,
+      cornerRadiusBL: 5,
+      cornerRadiusBR: 5,
+      width: am5.percent(100),
+      height: am5.percent(100),
+      templateField: "columnSettings",
+      
+    });
 
-var yAxis = chart.yAxes.push(
-  am5xy.CategoryAxis.new(root, {
-    renderer: yRenderer,
-    categoryField: "category"
-  })
-);
+    var circleTemplate = am5.Template.new({});
 
-var xRenderer = am5xy.AxisRendererX.new(root, {
-  visible: false,
-  minGridDistance: 30,
-  inversed: false,
-  opposite:true,
-});
-
-xRenderer.grid.template.set("visible", false, );
-
-var xAxis = chart.xAxes.push(
-  am5xy.CategoryAxis.new(root, {
-    renderer: xRenderer,
-    categoryField: "category",
-
-  })
-);
-
-
-// Create series
-// https://www.amcharts.com/docs/v5/charts/xy-chart/#Adding_series
-var series = chart.series.push(
-  am5xy.ColumnSeries.new(root, {
-    calculateAggregates: true,
-    stroke: am5.color(0xffffff),
-    clustered: false,
-    xAxis: xAxis,
-    yAxis: yAxis,
-    categoryXField: "x",
-    categoryYField: "y",
-    valueField: "value",
-  
-  })
-);
-
-series.columns.template.setAll({
-  tooltipText: "{value}",
-  strokeOpacity: 1,
-  strokeWidth: 2,
-  cornerRadiusTL: 5,
-  cornerRadiusTR: 5,
-  cornerRadiusBL: 5,
-  cornerRadiusBR: 5,
-  width: am5.percent(100),
-  height: am5.percent(100),
-  templateField: "columnSettings",
-  
-});
-
-var circleTemplate = am5.Template.new({});
-
-// Add heat rule
-// https://www.amcharts.com/docs/v5/concepts/settings/heat-rules/
-series.set("heatRules", [{
-  target: circleTemplate,
-  dataField: "value",
-  key: "radius"
-}]);
+    // Add heat rule
+    // https://www.amcharts.com/docs/v5/concepts/settings/heat-rules/
+    series.set("heatRules", [{
+      target: circleTemplate,
+      dataField: "value",
+      key: "radius"
+    }]);
 
 
 
-series.bullets.push(function () {
-  return am5.Bullet.new(root, {
-    sprite: am5.Label.new(root, {
-      fill: am5.color(0xffffff),
-      populateText: true,
-      centerX: am5.p50,
-      centerY: am5.p50,
-      fontSize: 15,
-      text: "{value}"
-    })
-  });
-});
+    series.bullets.push(function () {
+      return am5.Bullet.new(root, {
+        sprite: am5.Label.new(root, {
+          fill: am5.color(0xffffff),
+          populateText: true,
+          centerX: am5.p50,
+          centerY: am5.p50,
+          fontSize: 15,
+          text: "{value}"
+        })
+      });
+    });
 
-var colors = {
-  critical: am5.color(0xca0101),
-  bad: am5.color(0xe17a2d),
-  medium: am5.color(0xe1d92d),
-  good: am5.color(0x5dbe24),
-  verygood: am5.color(0x0b7d03)
-};
+    var colors = {
+      critical: am5.color(0xca0101),
+      bad: am5.color(0xe17a2d),
+      medium: am5.color(0xe1d92d),
+      good: am5.color(0x5dbe24),
+      verygood: am5.color(0x0b7d03)
+    };
 
-// Set data
-// https://www.amcharts.com/docs/v5/charts/xy-chart/#Setting_data
-
-
-
-
+    // Set data
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/#Setting_data
 var data = [];
-/*
-for (let el = 0; el < datos.length; el++) {
+let a = {};
+let nuevoArreglo = [];
+for (let c = 0; c < datos.length; c++) {
+  var ele = datos[c];
+    let i=0
+    let r = 0
+  if (ele === datos[0]  ) 
+  {
+    console.log("Es cero, no agregues")
+    
+    a= {
+       x: ele.Cliente,
+       y: ele.GrupoConcepto,
+       movimientos : [{
+          tipo: ele.Movimiento,
+          cantidad: ele.Cantidad
+       }     
+       ]
+    }
+    nuevoArreglo.push(a);
+    console.log(nuevoArreglo)
+  }
   
-   data.push({
-     y: datos[el].Cliente,
-     x: datos[el].GrupoConcepto,
-     columnSettings: {
-       fill: colors.good
-     },
-     value: 0,
-     movimientos,
-     disponible : 0,
-     total : 0,
-     porcentaje : 0
-    })
+  else {
+    let xy = ele.Cliente + ele.GrupoConcepto
+    console.log(nuevoArreglo.length)
+    while (i < nuevoArreglo.length) {
+      var x = nuevoArreglo[i];
+      let y = x.x+x.y;
+      console.log(xy);
+      console.log(y);
+      if(xy === y){
+        console.log("coincide")
+        let e = 0
+        while (e < x.movimientos.length){
+                let z = x.movimientos[e];
+                console.log(z)
+                let j=z.tipo
+                let h=ele.Movimiento
+                if (j===h) { 
+                    z.cantidad = parseInt(z.cantidad) + parseInt(ele.Cantidad)
+                   console.log(z.cantidad)
+                }
+                e++
+                }
+        console.log("se repite")
+        r = 1
+        break
+      } i++ 
+      };
+      if (r===0){
+        a= {
+            x: ele.Cliente,
+            y: ele.GrupoConcepto,
+            movimientos : [{
+               tipo: ele.Movimiento,
+               cantidad: ele.Cantidad
+            }     
+            ]
+         }
+         nuevoArreglo.push(a);
+      }
+    }
+    console.log(nuevoArreglo)
 }
+
+data = nuevoArreglo
 
 console.log(data);
-*/
 
-series.data.setAll(data);
+    series.data.setAll(data);
 
-//Siteamos los datos que aparecen en el eje y
-let ejey = []
+    //Siteamos los datos que aparecen en el eje y
+    let ejey = []
 
-for (let index = 0; index < clients.length; index++) {
-    const element = clients[index];
+    for (let index = 0; index < clients.length; index++) {
+        const element = clients[index];
 
-    //console.log(element.nombre);
-    ejey.push({category: element.nombre});
+        //console.log(element.nombre);
+        ejey.push({category: element.nombre});
 
+    }
+
+    yAxis.data.setAll(ejey);
+
+    //Siteamos los datos que aparecen en el eje x
+    let ejex = [];
+    for (let i = 0; i < grupo_conceptos.length; i++) {
+        const e = grupo_conceptos[i];
+      // console.log(e.nombre);
+        ejex.push({category: e.nombre});
+    }
+
+    xAxis.data.setAll(ejex);
+
+    var legend = chart.children.push(am5.Legend.new(root, {
+      centerX: am5.p50,
+      x: am5.p50
+    }));
+
+    legend.data.setAll(chart.series.values);
+
+    // Make stuff animate on load
+    // https://www.amcharts.com/docs/v5/concepts/animations/#Initial_animation
+    chart.appear(1000, 100);
 }
 
-yAxis.data.setAll(ejey);
-
-//Siteamos los datos que aparecen en el eje x
-let ejex = [];
-for (let i = 0; i < grupo_conceptos.length; i++) {
-    const e = grupo_conceptos[i];
-   // console.log(e.nombre);
-    ejex.push({category: e.nombre});
 }
-
-xAxis.data.setAll(ejex);
-
-var legend = chart.children.push(am5.Legend.new(root, {
-  centerX: am5.p50,
-  x: am5.p50
-}));
-
-legend.data.setAll(chart.series.values);
-
-// Make stuff animate on load
-// https://www.amcharts.com/docs/v5/concepts/animations/#Initial_animation
-chart.appear(1000, 100);
-
-}}
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -219,5 +262,6 @@ chart.appear(1000, 100);
 
 
 <template>
+<pre>{{cantidades}}</pre>
   <div class="graph" ref="chartdiv">  </div>
 </template>
