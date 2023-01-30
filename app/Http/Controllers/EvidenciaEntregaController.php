57e9152d50e3df1fc49065794fc00d91b977ed06
Hector <programadorjr@coorsamexico.com>
2023-01-30 12:44:41 -0600

    new migrations

---
+++ app/Http/Controllers/EvidenciaEntregaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EvidenciaEntrega;
+use Illuminate\Http\Request;
+
+class EvidenciaEntregaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EvidenciaEntrega  $evidenciaEntrega
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EvidenciaEntrega $evidenciaEntrega)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EvidenciaEntrega  $evidenciaEntrega
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EvidenciaEntrega $evidenciaEntrega)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EvidenciaEntrega  $evidenciaEntrega
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EvidenciaEntrega $evidenciaEntrega)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EvidenciaEntrega  $evidenciaEntrega
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EvidenciaEntrega $evidenciaEntrega)
+    {
+        //
+    }
+}
