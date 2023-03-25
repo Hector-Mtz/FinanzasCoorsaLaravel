8eccab9222950414497e2dd1fa9b176997b779ec
Hector <programadorjr@coorsamexico.com>
2023-03-25 13:56:33 -0600

    last changes presupuestos

--- resources/js/Pages/Presupuestos/PresupuestosIndex.vue
+++ resources/js/Pages/Presupuestos/PresupuestosIndex.vue
@@ -4,11 +4,11 @@ import { Inertia } from "@inertiajs/inertia";
 import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
 import { onMounted, reactive, ref, watch, computed } from "vue";
 import axios from "axios";
-
-
 /*Importacion de componentes*/
 import ButtonsGroup from "./Partials/ButtonsGroup.vue";
 import GraficaPresupuestos from "./Partials/GraficaPresupuestos.vue";
+import ButtonAdd from '@/Components/ButtonAdd.vue';
+import DangerButton from '@/Components/DangerButton.vue';
 /**/
 
 var props = defineProps({
@@ -72,6 +72,15 @@ const setMovimiento = (movimiento) =>
 
 const arregloValores = computed(() => {
   let arregloAux = [];
+  // define colors
+  var colors = {
+        "critical": "#ca0101",
+        "bad": "#e17a2d",
+        "medium": "#e1d92d",
+        "good": "#5dbe24",
+        "verygood": "#0b7d03"
+    };
+    
   for (let index = 0; index < props.clientes_cecos.length; index++) //recorremos todos los clientes
   {
      let cliente = props.clientes_cecos[index];
@@ -82,13 +91,22 @@ const arregloValores = computed(() => {
            ClienteName:cliente.nombre,
            GrupoConId:null,
            GrupoConName:null,
-           valor: 0
+           valor: 0,
+           color:null,
+           children:[]
         };
         let grupoConcepto = props.grupoConceptos_conceptos[index2];
         newObj.GrupoConId = grupoConcepto.id;
         newObj.GrupoConName = grupoConcepto.nombre;
         arregloAux.push(newObj);
      }   
+  }
+
+  for (let index = 0; index < props.grupoConceptos_conceptos.length; index++) 
+  {
+    const element = props.grupoConceptos_conceptos[index];
+    console.log(element);
+    
   }
 
  for (let index3 = 0; index3 < arregloAux.length; index3++)
@@ -100,6 +118,26 @@ const arregloValores = computed(() => {
         if(element.ClienteId == soli_gastos.clientes_id && element.GrupoConId == soli_gastos.grupo_concepto_id)
         {
             element.valor = soli_gastos.productos[0].total;
+             if(element.valor <=300)
+             {
+               element.color = colors.critical
+             }
+             if(element.valor >300 && element.valor <= 500)
+             {
+                element.valor = colors.bad
+             }
+             if(element.valor >500 && element.valor <= 800)
+             {
+                element.color = colors.medium
+             }
+             if(element.valor >800 && element.valor <= 5000)
+             {
+               element.color = colors.good
+             }
+             if(element.valor > 5000)
+             {
+                element.color = colors.verygood
+             }
         }
     }  
   }
@@ -142,18 +180,14 @@ const setFor = (tipoAcomodo) =>
             </div>
         </template>
         <div class="grid grid-cols-6 grid-rows-2">
-            <ButtonsGroup @seleccion="setMovimiento" @setFor="setFor" class="justify-center col-start-2 row-start-1 row-end-3"/>
+            <ButtonsGroup @seleccion="setMovimiento" @setFor="setFor" class="justify-center col-start-1 row-start-1 row-end-3"/>
+            <DangerButton>
+                Tabla de datos
+            </DangerButton>
         </div>
         <div class="py-12">
             <!--Grafica-->
             <GraficaPresupuestos :arregloValores = "arregloValores" />
-            <!--
-            <div class="pt-2 pb-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden shadow-xl sm:rounded-lg">
-                
-               </div>
-            </div>
-        -->
         </div>
     </AppLayout>
 </template>
